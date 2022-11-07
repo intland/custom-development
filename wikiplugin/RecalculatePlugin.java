089833f92398457091d30a1b53c80835da64d581
Patrik Pasztor <patrik.pasztor@intland.com>
2022-11-07 09:18:46 +0100

    Added description and current date to RecalculatePlugin

--- wikiplugin/RecalculatePlugin.java
+++ wikiplugin/RecalculatePlugin.java
@@ -4,22 +4,28 @@ import java.util.Map;
 import com.ecyrd.jspwiki.WikiContext;
 import com.intland.codebeamer.controller.support.computedfield.ComputedFieldSupport;
 import com.intland.codebeamer.manager.TrackerManager;
-import com.intland.codebeamer.persistence.dto.ReadOnlyTrackerItemDto;
 import com.intland.codebeamer.persistence.dto.TrackerDto;
 import com.intland.codebeamer.wiki.CodeBeamerWikiContext;
 import com.intland.codebeamer.wiki.plugins.base.AbstractCodeBeamerWikiPlugin;
+
+/**
+ * Enables the user to trigger the recalculation of all computed fields of items in a tracker.
+ * The ID of the tracker have to be passed as parameter.
+ * Whenever the plugin loads, the recalculation will start, so run it only when necessary to prevent performance issues
+ * when working with giant trackers with many items and computed fields.
+ */
 
 public class RecalculatePlugin extends AbstractCodeBeamerWikiPlugin {
 
     public String execute(WikiContext wikicontext, Map params) {
 
-        ReadOnlyTrackerItemDto subject = (ReadOnlyTrackerItemDto) ((CodeBeamerWikiContext) wikicontext).getOwner();
+        Integer trackerId = Integer.parseInt((String)params.get("trackerId"));
 
-        TrackerDto trackerDto = TrackerManager.getInstance().findById(((CodeBeamerWikiContext) wikicontext).getUser(), subject.getTracker().getId());
+        TrackerDto trackerDto = TrackerManager.getInstance().findById(((CodeBeamerWikiContext) wikicontext).getUser(), trackerId);
 
         ComputedFieldSupport computedFieldSupport = getApplicationContext(wikicontext).getBean(ComputedFieldSupport.class);
         computedFieldSupport.recalculateItemComputedFieldsInTracker(trackerDto, false);
 
-        return "recalculated";
+        return "Recalculated: " + new java.util.Date();
     }
 }
